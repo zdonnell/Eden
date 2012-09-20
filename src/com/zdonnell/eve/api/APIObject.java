9c244b2326b11569d991538fb4105a93fd1866d7
Zach Donnell <slick50zd1@gmail.com>
2012-09-20 11:26:57 -0700

    Shoudl have been in last commit

---
+++ src/com/zdonnell/eve/api/APIObject.java
@@ -0,0 +1,11 @@
+package com.zdonnell.eve.api;
+
+
+public class APIObject {
+	
+	protected APICredentials credentials;
+	
+	protected void setCredentials(APICredentials credentials) {
+		this.credentials = credentials;
+	}
+}
