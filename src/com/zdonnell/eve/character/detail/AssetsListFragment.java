eabaedf33f3731574037188674b5cad5adaba076
Zach Donnell <slick50zd1@gmail.com>
2012-11-14 16:01:52 -0800

    More work on assets

--- src/com/zdonnell/eve/character/detail/AssetsListFragment.java
+++ src/com/zdonnell/eve/character/detail/AssetsListFragment.java
@@ -1,9 +1,11 @@
 package com.zdonnell.eve.character.detail;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.SparseArray;
@@ -32,6 +34,8 @@ public class AssetsListFragment extends Fragment {
     private Context context;
     
     GridView assetsGridView;
+    
+    private ImageService imageService;
         
     /**
      * Constructor
@@ -53,6 +57,8 @@ public class AssetsListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
     {
     	context = inflater.getContext();
+    	imageService = ImageService.getInstance(context);
+    	
     	
     	LinearLayout inflatedView = (LinearLayout) inflater.inflate(R.layout.char_detail_assets, container, false);
     	assetsGridView = (GridView) inflatedView.findViewById(R.id.char_detail_assets_list);
@@ -73,10 +79,34 @@ public class AssetsListFragment extends Fragment {
     private class AssetsStationsListAdapter extends ArrayAdapter<AssetsEntity>
     {
     	int resourceID;
+    	
+    	SparseArray<String> typeNames;
+    	
+    	HashMap<TextView, Integer> typeNameMappings = new HashMap<TextView, Integer>();
+    	
+    	boolean typeNamesLoaded = false;
     	    	    	
     	public AssetsStationsListAdapter(Context context, int textViewResourceID, AssetsEntity[] assetLocations) {
 			super(context, textViewResourceID, assetLocations);
 			this.resourceID = textViewResourceID;
+			
+			typeNames = new SparseArray<String>(assetLocations.length);
+			
+			if (assetLocations[0] instanceof AssetsEntity.Item)
+			{
+				final int[] typeIDs = new int[assetLocations.length];
+				for (int x = 0; x < assetLocations.length; x++) typeIDs[x] = assetLocations[x].attributes().typeID;
+				
+				new Eve(context).getTypeName(new APICallback<String[]>()
+				{
+					@Override
+					public void onUpdate(String[] retTypeNames) 
+					{
+						for (int i = 0; i < typeIDs.length; i++) typeNames.put(typeIDs[i], retTypeNames[i]);
+						updateDisplayedViews();
+					}
+				}, typeIDs);
+			}
 		}
 		
 		@Override
@@ -90,18 +120,14 @@ public class AssetsListFragment extends Fragment {
 			/* Determine if we recyle the old view, or inflate a new one */
 			if (convertView == null) itemView = (LinearLayout) inflater.inflate(resourceID, parent, false);
 			else itemView = (LinearLayout) convertView;
-				
-			/* Grab references to the views needing update */
-			TextView text = (TextView) itemView.findViewById(R.id.char_detail_assets_list_item_name);
 			
 			if (assetsEntity instanceof AssetsEntity.Station)
 			{
-				AssetsEntity.Station station = (AssetsEntity.Station) assetsEntity;
-				text.setText("LocationID: " + station.getLocationID());			
+				setupStation(itemView, (AssetsEntity.Station) getItem(position));
 			}
 			else if (assetsEntity instanceof AssetsEntity.Item)
 			{
-				
+				setupAsset(itemView, (AssetsEntity.Item) getItem(position));
 			}
 				
 			itemView.setOnClickListener(new View.OnClickListener() 
@@ -115,7 +141,7 @@ public class AssetsListFragment extends Fragment {
 						AssetsEntity[] assetsAsArray = new AssetsEntity[containedAssets.size()];
 						containedAssets.toArray(assetsAsArray);
 						
-						AssetsListAdapter assetsAdapter = new AssetsListAdapter(context, R.layout.char_detail_assets_list_item, assetsAsArray);
+						AssetsStationsListAdapter assetsAdapter = new AssetsStationsListAdapter(context, R.layout.char_detail_assets_list_item, assetsAsArray);
 						assetsGridView.setAdapter(assetsAdapter);
 					}
 				}
@@ -123,83 +149,64 @@ public class AssetsListFragment extends Fragment {
 			
 			return itemView;
 		}
-    }
-    
-    private class AssetsListAdapter extends ArrayAdapter<AssetsEntity>
-    {
-    	int resourceID;
-    	
-    	private AssetsEntity[] assetsList;
-    	
-    	public AssetsListAdapter(Context context, int textViewResourceID, AssetsEntity[] assetLocations) {
-			super(context, textViewResourceID, assetLocations);
-			this.resourceID = textViewResourceID;
-			this.assetsList = assetLocations;
+		
+		private void updateDisplayedViews()
+		{
+			typeNamesLoaded = true;
+			
+			/* Update Type Names */
+			for (TextView textView : typeNameMappings.keySet())
+			{
+				int typeID = typeNameMappings.get(textView);
+				textView.setText(typeNames.get(typeID));
+			}
 		}
-    			
-		@Override
-		public View getView(final int position, View convertView, ViewGroup parent)
-		{			
-			LinearLayout itemView; 
-			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			
-			/* Determine if we recyle the old view, or inflate a new one */
-			if (convertView == null) itemView = (LinearLayout) inflater.inflate(resourceID, parent, false);
-			else itemView = (LinearLayout) convertView;
-				
+		
+		private void setupStation(LinearLayout rootView, AssetsEntity.Station station)
+		{
 			/* Grab references to the views needing update */
-			final TextView text = (TextView) itemView.findViewById(R.id.char_detail_assets_list_item_name);
-			final ImageView icon = (ImageView) itemView.findViewById(R.id.char_detail_assets_list_item_typeIcon);
-			final TextView quantity = (TextView) itemView.findViewById(R.id.char_detail_assets_list_item_count);
+			final TextView text = (TextView) rootView.findViewById(R.id.char_detail_assets_list_item_name);
+			final TextView quantity = (TextView) rootView.findViewById(R.id.char_detail_assets_list_item_count);
 			
-			new Eve(context).getTypeName(new APICallback<String[]>(){
-
-				@Override
-				public void onUpdate(String[] updatedData) {
-					text.setText(updatedData[0]);
-				}
-				
-			}, new int[] { assetsList[position].attributes().typeID });
+			text.setText("LocationID: " + station.getLocationID());
 			
-			text.setText("typeID: " + assetsList[position].attributes().typeID);
-			itemView.setOnClickListener(new View.OnClickListener() 
+			quantity.setText(String.valueOf(station.getContainedAssets().size()));
+		}
+		
+		private void setupAsset(LinearLayout rootView, AssetsEntity.Item item)
+		{
+			boolean isPackaged = !item.attributes().singleton;
+			int typeID = item.attributes().typeID;
+			int count = item.attributes().quantity;
+			
+			/* Grab references to the views needing update */
+			final TextView text = (TextView) rootView.findViewById(R.id.char_detail_assets_list_item_name);
+			final ImageView icon = (ImageView) rootView.findViewById(R.id.char_detail_assets_list_item_typeIcon);
+			final TextView quantity = (TextView) rootView.findViewById(R.id.char_detail_assets_list_item_count);
+			final LinearLayout iconBorder = (LinearLayout) rootView.findViewById(R.id.char_detail_assets_list_item_typeIconBorder);
+			
+			typeNameMappings.put(text, typeID);
+			if (typeNamesLoaded) text.setText(typeNames.get(typeID));
+			
+			icon.setTag(typeID);
+			
+			ImageService.getInstance(context).getTypes(new ImageService.IconObtainedCallback() 
 			{	
 				@Override
-				public void onClick(View v) 
+				public void iconsObtained(SparseArray<Bitmap> bitmaps) 
 				{
-					if (assetsList[position].containsAssets())
-					{
-						ArrayList<AssetsEntity> containedAssets = assetsList[position].getContainedAssets();
-						AssetsEntity[] assetsAsArray = new AssetsEntity[containedAssets.size()];
-						containedAssets.toArray(assetsAsArray);
-						
-						AssetsListAdapter assetsAdapter = new AssetsListAdapter(context, R.layout.char_detail_assets_list_item, assetsAsArray);
-						assetsGridView.setAdapter(assetsAdapter);
-					}
-				}
-			});
-			
-			icon.setTag(assetsList[position].attributes().typeID);
-			
-			new ImageService(context).getTypes(new ImageService.IconObtainedCallback() {
-				
-				@Override
-				public void iconsObtained(SparseArray<Bitmap> bitmaps) {
 					if (bitmaps.get((Integer) icon.getTag()) != null);
 					{
-						icon.setImageBitmap(bitmaps.valueAt(0));
 						icon.setLayoutParams(new LinearLayout.LayoutParams(icon.getWidth(), icon.getWidth()));
+						icon.setImageBitmap(bitmaps.valueAt(0));
 					}
 				}
-			}, assetsList[position].attributes().typeID);
+			}, typeID);
 			
-			if (assetsList[position].attributes().singleton) quantity.setVisibility(View.INVISIBLE);
-			else
-			{
-				quantity.setText(assetsList[position].attributes().quantity + "");
-			}
+			quantity.setVisibility(isPackaged ? View.VISIBLE : View.INVISIBLE);
+			iconBorder.setBackgroundColor(isPackaged ? Color.parseColor("#666666") : Color.parseColor("#aaaaaa"));
 			
-			return itemView;
+			if (isPackaged) quantity.setText(String.valueOf(count));
 		}
     }
 }
