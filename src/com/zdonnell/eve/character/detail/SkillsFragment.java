a12d448ef0d87ff9d03466894b27b21b98f3ff5c
Zach Donnell <slick50zd1@gmail.com>
2013-03-06 22:19:03 -0800

    Switched Skills View to ExpandableListView
    was previously (incorrectly) a standard ListView

--- src/com/zdonnell/eve/character/detail/SkillsFragment.java
+++ src/com/zdonnell/eve/character/detail/SkillsFragment.java
@@ -2,6 +2,7 @@ package com.zdonnell.eve.character.detail;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.AttributeSet;
@@ -9,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -47,7 +50,7 @@ public class SkillsFragment extends Fragment {
     
     private SkillGroup[] skillTree;
     
-    private ListView skillsListView;
+    private ExpandableListView skillsListView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
@@ -55,7 +58,7 @@ public class SkillsFragment extends Fragment {
     	context = inflater.getContext();
     	LinearLayout inflatedView = (LinearLayout) inflater.inflate(R.layout.char_detail_skills, container, false);
     	
-    	skillsListView = (ListView) inflatedView.findViewById(R.id.char_detail_skills_list);
+    	skillsListView = (ExpandableListView) inflatedView.findViewById(R.id.char_detail_skills_list);
     	
     	character = new APICharacter(new APICredentials(getArguments().getInt("keyID"), getArguments().getString("vCode")), getArguments().getInt("characterID"), context);
     	
@@ -74,95 +77,161 @@ public class SkillsFragment extends Fragment {
     
     private void updateSkillList()
     {
-    	skillsListView.setAdapter(new SkillsListAdapter(context, R.layout.char_detail_skills_list_item, skillTree, null));
+    	skillsListView.setAdapter(new SkillsExpandedListAdapter(context, skillTree));
     }
     
-    /**
-     * {@link ArrayAdapter} subclass to populate the Skill Listing {@link ListView}
-     * 
-     * @author Zach
-     *
-     */
-    private class SkillsListAdapter extends ArrayAdapter<SkillGroup>
-	{	    	
-    	/**
-		 * ID of the resource to inflate for the entire row
-		 */
-		int resourceID;
-		
-		private int displayMode = TRAINED_SKILLS;
-				
-		/**
-		 * Constructor
-		 * 
-		 * @param context
-		 * @param viewResourceID the ID of the layout resource to use as a row in the Attributes {@link ListView}
-		 */
-		public SkillsListAdapter(Context context, int viewResourceID, SkillGroup[] skillTree, Skill[] characterSkills) 
-		{
-			super(context, viewResourceID, skillTree);			
-			this.resourceID = viewResourceID;
-		}
-		
-		public void setDisplayMode(int displayMode)
-		{
-			this.displayMode = displayMode;
-			notifyDataSetChanged();
-		}
-		
+    private class SkillsExpandedListAdapter implements ExpandableListAdapter
+    {
+        private Context context;
+        
+        private SkillGroup[] skillTree;
+        
+		LayoutInflater inflater;
+        
+        private static final int groupLayoutID = R.layout.char_detail_skills_list_item;
+        private static final int childLayoutID = R.layout.char_detail_skills_list_item_subskill;
+
+        public SkillsExpandedListAdapter(Context context, SkillGroup[] skillTree) 
+        {
+            this.context = context;
+            this.skillTree = skillTree;
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+    	
 		@Override
-		public View getView(final int position, View convertView, ViewGroup parent)
-		{			
-			LinearLayout preparedView; 
-			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		public boolean areAllItemsEnabled() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public Object getChild(int groupPosition, int childPosition) 
+		{
+			return skillTree[groupPosition].containedSkills()[childPosition];
+		}
+
+		@Override
+		public long getChildId(int groupPosition, int childPosition) 
+		{
+			return skillTree[groupPosition].containedSkills()[childPosition].typeID();
+		}
+
+		@Override
+		public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) 
+		{
+			View preparedView;
 			
-			/* Determine if we recycle the old view, or inflate a new one */
-			if (convertView == null) preparedView = (LinearLayout) inflater.inflate(resourceID, parent, false);
-			else preparedView = (LinearLayout) convertView;
-				
-			SkillGroup skillGroup = getItem(position);
+			if (convertView != null) preparedView = convertView;
+			else preparedView = inflater.inflate(childLayoutID, parent, false);
+			
+			SkillInfo skillInfo = (SkillInfo) getChild(groupPosition, childPosition);
+			prepareChild(skillInfo, preparedView);
+			
+			return preparedView;
+		}
+		
+		protected void prepareChild(SkillInfo skillInfo, View preparedView)
+		{
+			TextView skillName = (TextView) preparedView.findViewById(R.id.char_detail_skills_list_item_skillname);
+			skillName.setText(skillInfo.typeName());
+		}
+
+		@Override
+		public int getChildrenCount(int groupPosition) 
+		{
+			return skillTree[groupPosition].containedSkills().length;
+		}
+
+		@Override
+		public long getCombinedChildId(long groupId, long childId)
+		{
+			return childId;
+		}
+
+		@Override
+		public long getCombinedGroupId(long groupId) 
+		{
+			return groupId;
+		}
+
+		@Override
+		public Object getGroup(int groupPosition) 
+		{
+			return skillTree[groupPosition];
+		}
+
+		@Override
+		public int getGroupCount() 
+		{
+			return skillTree.length;
+		}
+
+		@Override
+		public long getGroupId(int groupPosition) 
+		{
+			return skillTree[groupPosition].groupID();
+		}
+
+		@Override
+		public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) 
+		{
+			View preparedView;
+			
+			if (convertView != null) preparedView = convertView;
+			else preparedView = inflater.inflate(groupLayoutID, parent, false);
+			
+			SkillGroup skillGroup = (SkillGroup) getGroup(groupPosition);
 			prepareGroup(skillGroup, preparedView);
 			
 			return preparedView;
 		}
 		
-		private void prepareGroup(final SkillGroup skillGroup, LinearLayout preparedView)
+		protected void prepareGroup(final SkillGroup skillGroup, View preparedView)
 		{
-			TextView groupName = (TextView) preparedView.findViewById(R.id.char_detail_skills_list_item_groupName);
-			final LinearLayout subSkillWrapper = (LinearLayout) preparedView.findViewById(R.id.char_detail_skills_list_item_group_skillContainer);
-			
-			preparedView.setOnClickListener(new View.OnClickListener()
-			{
-				@Override
-				public void onClick(View v) 
-				{
-					switch (subSkillWrapper.getVisibility())
-					{
-					case View.VISIBLE:
-						subSkillWrapper.setVisibility(View.GONE);
-						subSkillWrapper.removeAllViews();
-
-						break;
-					case View.GONE:
-						subSkillWrapper.setVisibility(View.VISIBLE);
-						
-						for (SkillInfo skillInfo : skillGroup.containedSkills())
-						{
-							if (skillInfo.isPublished())
-							{
-								LinearLayout skillLayout = (LinearLayout) View.inflate(context, R.layout.char_detail_skills_list_item_subskill, subSkillWrapper);
-								
-								TextView skillName = (TextView) skillLayout.findViewById(R.id.char_detail_skills_list_item_skillname);
-								skillName.setText(skillInfo.typeName());
-								/*subSkillWrapper.addView(skillName);*/
-							}
-						}
-						break;
-					}
-				}
-			});
-			
+			TextView groupName = (TextView) preparedView.findViewById(R.id.char_detail_skills_list_item_groupName);			
 			groupName.setText(skillGroup.groupName());
 		}
-	}
+
+		@Override
+		public boolean hasStableIds() 
+		{
+			return true;
+		}
+
+		@Override
+		public boolean isChildSelectable(int groupPosition, int childPosition) 
+		{
+			return false;
+		}
+
+		@Override
+		public boolean isEmpty() 
+		{
+			return skillTree.length == 0;
+		}
+
+		@Override
+		public void onGroupCollapsed(int groupPosition) 
+		{
+			
+		}
+
+		@Override
+		public void onGroupExpanded(int groupPosition) 
+		{
+						
+		}
+
+		@Override
+		public void registerDataSetObserver(DataSetObserver observer) 
+		{
+
+		}
+
+		@Override
+		public void unregisterDataSetObserver(DataSetObserver observer) 
+		{
+			
+		}
+    }
 }
