be5677e17736e7b5b5f9b6c72ac91a0f239440fb
Zach Donnell <slick50zd1@gmail.com>
2013-04-09 20:29:47 -0700

    Commit

--- src/com/zdonnell/eve/CharacterSheetActivity.java
+++ src/com/zdonnell/eve/CharacterSheetActivity.java
@@ -1,11 +1,22 @@
 package com.zdonnell.eve;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.util.SparseArray;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.zdonnell.eve.api.APICredentials;
+import com.zdonnell.eve.api.ImageService;
+import com.zdonnell.eve.api.ImageService.IconObtainedCallback;
 import com.zdonnell.eve.api.character.APICharacter;
 import com.zdonnell.eve.character.detail.AttributesFragment;
 import com.zdonnell.eve.character.detail.ParentAssetsFragment;
@@ -104,5 +115,68 @@ public class CharacterSheetActivity extends BaseActivity
             detailIntent.putExtra("character", characterInfo);
             startActivity(detailIntent);
         }
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+    	super.onCreateOptionsMenu(menu);
+    	
+    	MenuInflater menuInflater = getMenuInflater(); 
+    	menuInflater.inflate(R.menu.character_sheet, menu);
+    	
+    	return true;
+    }
+    
+    public boolean onOptionsItemSelected (MenuItem item) {
+    	
+	    switch (item.getItemId())
+	    {
+	    case R.id.notification_toggle:
+	    	ImageService.getInstance(this).getPortraits(new IconObtainedCallback()
+	    	{
+				@Override
+				public void iconsObtained(SparseArray<Bitmap> bitmaps) 
+				{
+			    	buildNotification(bitmaps.valueAt(0));
+				}
+	    	}, true, assembledChar.id());
+	    	
+	    	break;
+	    }
+		return true;
+    }
+    
+    private void buildNotification(Bitmap charPortrait)
+    {
+    	NotificationCompat.Builder mBuilder =
+    	        new NotificationCompat.Builder(this)
+    	        .setSmallIcon(R.drawable.ic_launcher)
+    	        .setLargeIcon(charPortrait)
+    	        .setContentTitle("My notification")
+    	        .setOngoing(true)
+    	        .setContentText("Hello World!");
+    	// Creates an explicit intent for an Activity in your app
+    	Intent resultIntent = new Intent(this, CharactersActivity.class);
+
+    	// The stack builder object will contain an artificial back stack for the
+    	// started Activity.
+    	// This ensures that navigating backward from the Activity leads out of
+    	// your application to the Home screen.
+    	TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+    	// Adds the back stack for the Intent (but not the Intent itself)
+    	stackBuilder.addParentStack(CharactersActivity.class);
+    	// Adds the Intent that starts the Activity to the top of the stack
+    	stackBuilder.addNextIntent(resultIntent);
+    	PendingIntent resultPendingIntent =
+    	        stackBuilder.getPendingIntent(
+    	            0,
+    	            PendingIntent.FLAG_UPDATE_CURRENT
+    	        );
+    	mBuilder.setContentIntent(resultPendingIntent);
+    	NotificationManager mNotificationManager =
+    	    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    	// mId allows you to update the notification later on.
+    	mNotificationManager.notify(0, mBuilder.build());
     }
 }
