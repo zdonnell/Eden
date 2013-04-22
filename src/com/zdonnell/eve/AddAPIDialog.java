dcc1c6058db521b3d17071c24006dacca7545483
Liran Nuna <liranuna@gmail.com>
2013-04-21 19:57:01 -0700

    Do not enable the 'Get Characters' button unless both keyId and vCode are set

--- src/com/zdonnell/eve/AddAPIDialog.java
+++ src/com/zdonnell/eve/AddAPIDialog.java
@@ -7,6 +7,8 @@ import android.app.Dialog;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
@@ -26,6 +28,90 @@ import com.zdonnell.eve.api.account.EveCharacter;
 
 public class AddAPIDialog extends DialogFragment 
 {
+	/**
+	 * Manages a list of TextWatchers that, when all not empty will enable a button
+	 */
+	private static class EnableButtonMultiWatcher {
+		/**
+		 * Watches for empty text 
+		 */
+		private class EmptyTextWatcher implements TextWatcher
+		{
+			// Assume invalid at start
+			private boolean valid = false;
+			
+			@Override
+			public void afterTextChanged(Editable editable)
+			{
+				// Text is valid when the string isn't empty
+				valid = (editable.length() != 0);
+				
+				EnableButtonMultiWatcher.this.check();
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after)
+			{
+				// Do nothing
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count)
+			{
+				// Do nothing
+			}
+			
+			public boolean isValid()
+			{
+				return valid;
+			}
+			
+			public void setValid(boolean valid)
+			{
+				this.valid = valid;
+			}
+		}
+		
+		private ArrayList<EmptyTextWatcher> watchers = new ArrayList<EmptyTextWatcher>();
+		private Button button;
+		
+		public EnableButtonMultiWatcher(Button button)
+		{
+			this.button = button;
+			
+			// First check
+			check();
+		}
+		
+		public TextWatcher createWatcher()
+		{
+			EmptyTextWatcher newWatcher = new EmptyTextWatcher();
+			watchers.add(newWatcher);
+			
+			return newWatcher;
+		}
+		
+		public void addTextView(TextView textView)
+		{
+			EmptyTextWatcher watcher = (EmptyTextWatcher)createWatcher();
+			
+			textView.addTextChangedListener(watcher);
+			watcher.setValid(textView.getText().length() != 0);
+			
+			check();
+		}
+		
+		public void check()
+		{
+			// Assume valid until one is invalid
+			boolean valid = true;
+			for(EmptyTextWatcher watcher : watchers)
+				valid &= watcher.isValid();
+			
+			button.setEnabled(valid);
+		}
+	}
+	
 	APICredentials[] keys;
 	
 	SparseArray<ArrayList<EveCharacter>> characters = new SparseArray<ArrayList<EveCharacter>>();
@@ -72,9 +158,13 @@ public class AddAPIDialog extends DialogFragment
 		
 		getCharsButton = (Button) root.findViewById(R.id.characters_add_characters_get_button);
 		addCharsButton = (Button) root.findViewById(R.id.characters_add_characters_add_button);
-		
+
+		final EnableButtonMultiWatcher getCharsWatcher = new EnableButtonMultiWatcher(getCharsButton);
 		final EditText keyIDField = (EditText) root.findViewById(R.id.characters_add_characters_api_field);
 		final EditText vCodeField = (EditText) root.findViewById(R.id.characters_add_characters_vcode_field);
+		
+		getCharsWatcher.addTextView(keyIDField);
+		getCharsWatcher.addTextView(vCodeField);
 		
 		getCharsButton.setOnClickListener(new View.OnClickListener() 
 		{
