0d64ebf55fff9f8c06d90767e127dc6394343e2d
Zach Donnell <slick50zd1@gmail.com>
2013-06-07 22:19:39 -0700

    Set up UIL implementation requirements

---
+++ src/com/zdonnell/eve/EdenApplication.java
@@ -0,0 +1,37 @@
+package com.zdonnell.eve;
+
+import java.io.File;
+
+import android.app.Application;
+
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.utils.StorageUtils;
+
+public class EdenApplication extends Application {
+	
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        File cacheDir = StorageUtils.getCacheDirectory(getBaseContext());
+        
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+        .cacheInMemory()
+        .cacheOnDisc()
+        .build();
+        
+        // Create global configuration and initialize ImageLoader with this configuration
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+        .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+        .memoryCacheSize(2 * 1024 * 1024)
+        .discCache(new UnlimitedDiscCache(cacheDir)) // default
+        .defaultDisplayImageOptions(defaultOptions) // default
+        .build();
+        
+        ImageLoader.getInstance().init(config);
+    }
+}
